803731627f1dbbc0302d0bff78d6f46f98b5d9d2
pixel <alex.pixelwinner@gmail.com>
2023-01-02 17:30:37 +0200

    02.01.23

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,17 @@
-import {configureStore} from '@reduxjs/toolkit'
-import {userSlice} from "./reducers/UserReducer";
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {userApi} from "./api/UserApi";
-import {tablesApi} from "./api/TablesApi";
-import {tablesSlice} from "./reducers/TablesReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { userSlice } from "./reducers/UserReducer";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { userApi } from "./api/UserApi";
+import { tablesApi } from "./api/TablesApi";
 
 export const store = configureStore({
     reducer: {
         userSlice: userSlice.reducer,
-        tablesSlice: tablesSlice.reducer,
         [userApi.reducerPath]: userApi.reducer,
         [tablesApi.reducerPath]: tablesApi.reducer
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(userApi.middleware, tablesApi.middleware),
-})
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware, tablesApi.middleware)
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
